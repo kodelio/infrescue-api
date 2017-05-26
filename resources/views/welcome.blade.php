d13fb22b65b9820d9029720cb1a24ea12108efe9
Laurent Toson <laurentt96@outlooK.fr>
2017-05-26 20:28:22 +0200

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,67 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('/home/images/favicon.ico') }}">
+    <title>InfRescue</title>
+    <meta name="description" content="Application InfRescue (Projet GL INFRES 9)">
+    <link rel="apple-touch-icon" href="{{ asset('/home/images/touch/apple-touch-icon.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/home/images/touch/apple-touch-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/home/images/touch/apple-touch-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/home/images/touch/apple-touch-icon-144x144.png') }}">
+    <link rel="stylesheet" href="{{ asset('/home/css/style.css') }}">
+    <!--[if lt IE 9]>
+    <script src="{{ asset('/home/js/html5.js') }}"></script>
+    <script src="{{ asset('/home/js/respond.js') }}"></script>
+    <![endif]-->
+</head>
+<body class="no-js">
+<div class="main">
+    <header>
+        <div class="wrap">
+            <img src="{{ asset('/home/upload/iphone.png') }}" height="532" width="252" alt="" class="header-img">
+            <div class="header-wrapper">
+                <h1 style="color: black;"><span>Inf</span>Rescue</h1>
+                <p style="color: black; font-size: 28px;">Gestion d'hopitaux de campagne</p>
+                <div class="buttons-wrapper">
+                    <a href="#" class="button" target="_blank">Télécharger l'application</a>
+                    <a style="color: black;" href="http://sonar.infrescue.cf" class="button button-stripe" target="_blank">SonarQube</a>
                 </div>
             </div>
+            <!-- /.header-wrapper -->
         </div>
-    </body>
+        <!-- /.wrap -->
+    </header>
+    <div class="spanning">
+        <!-- /.promo clearfix -->
+        <div class="discover clearfix">
+            <div class="wrap" style="">
+                <div class="clearfix" style="text-align: center;">
+                    <h1>Projet GL INFRES 9</h1>
+                    <br />
+                    <p>Projet réalisé pour le module Projet GL de la formation INFRES (Ecole des Mines d'Alès)
+                    <img src="{{ asset('/home/images/infrescue.png') }}" style="width: 45%;"/>
+                    </p>
+                </div>
+            </div>
+            <!-- /.wrap -->
+        </div>
+        <!-- /.discover clearfix -->
+    </div>
+    <!-- /.spanning-columns -->
+</div>
+<!-- /.main -->
+<footer>
+    <div class="wrap">
+        <p>&copy; 2017 <strong>InfRescue (Scott Rayapoullé, Raphael Haltz, Laurent Toson)</strong>, All Rights Reserved</p>
+    </div>
+    <!-- /.wrap -->
+</footer>
+<script src="{{ asset('/home/js/jquery.js') }}"></script>
+<script src="{{ asset('/home/js/library.js') }}"></script>
+<script src="{{ asset('/home/js/script.js') }}"></script>
+<script src="{{ asset('/home/js/retina.js') }}"></script>
+</body>
 </html>
